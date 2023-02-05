0bb2ef2fe51c42c396913b68cbb9a0b0d14176b8
MannyAmza <mannyamza@gmail.com>
2023-02-05 00:16:28 -0800

    image sync and fonts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Tour of the Universe';
+  title = 'Tour of the Stars';
 }
